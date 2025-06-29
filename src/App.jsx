e2758baf4e09a56383c5f0c578d9b14c707fcc49
Jack-Underhill <jack.underhill17@gmail.com>
2025-06-29 12:01:48 -0700

    View Count Displayed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { useEffect, useState } from 'react'
+
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
 import About from './components/About'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-import { useEffect, useState } from 'react'
+
+import clicks from './assets/clicks.svg'
 
 function App() {
   const [visitCount, setVisitCount] = useState(null);
@@ -19,7 +23,6 @@ function App() {
       setVisitCount(data.count);
       console.log(data.count);
     });
-    
   }, []);
 
   return (
@@ -31,6 +34,22 @@ function App() {
           backgroundImage: `radial-gradient(circle at 72% 50%, rgba(56,189,248,0.35) 0%, rgba(56,189,248,0.15) 40%, transparent 90%), url('/black-linen.png')`
         }}
       />
+
+      {/* View Count */}
+      <div 
+        className="m-3 sm:m-5 h-10 flex gap-2 items-center" 
+        title='User Visit Count' 
+        data-aos="flip-up"
+      >
+          <img 
+              src={clicks} 
+              alt={`View svg`}
+              className='h-full hover:animate-spin' 
+          />
+          <div className='text-md font-semibold text-emerald-50'>
+            {visitCount !== null ? `${visitCount} views` : 'Loading ...'}
+          </div>
+        </div>
 
       {/* Layout */}
       <div className='relative z-10 flex flex-col gap-30 sm:gap-35 lg:gap-0 px-10 sm:px-20 pb-40 pt-25 sm:pt-35 md:pt-25 lg:pt-0'>
