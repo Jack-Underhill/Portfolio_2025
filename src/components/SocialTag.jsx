2815e1766b3f3d10183901f5d21d4e1d68212298
Jack-Underhill <jack.underhill17@gmail.com>
2025-12-30 15:46:59 -0800

    Smoothened Social Tag Behavior

--- src/components/SocialTag.jsx
+++ src/components/SocialTag.jsx
@@ -1,20 +1,34 @@
-function SocialTag({ name, link, icon }) {
+import { useState, useCallback } from 'react'
+
+function SocialTag({ name, link, icon, aos = "fade-down-right" }) {
+    const [isHovered, setIsHovered] = useState(false)
+
+    const onEnter = useCallback(() => setIsHovered(true), [])
+    const onLeave = useCallback(() => setIsHovered(false), [])
+
     return (
-        <div className="h-20 w-20" data-aos="fade-down-right">
-            <a 
-                className='h-full' 
-                href={link} 
-                target="_blank" 
+        <div className="h-20 w-20" data-aos={aos}>
+            <a
+                className="h-full inline-block"
+                href={link}
+                target="_blank"
+                rel="noopener noreferrer"
                 title={`View My ${name}`}
+                onPointerEnter={onEnter}
+                onPointerLeave={onLeave}
             >
-                <img 
-                    src={icon} 
+                <img
+                    src={icon}
                     alt={`${name} Profile`}
-                    className='h-full hover:animate-bounce' 
+                    draggable={false}
+                    className={[
+                        "h-full",
+                        isHovered ? "animate-bounce will-change-transform" : "",
+                    ].join(" ")}
                 />
             </a>
         </div>
     )
 }
 
-export default SocialTag;
+export default SocialTag
